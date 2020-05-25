d9cd8f1d8f5ab49490edcbe4cbc23cc3d03ec5df
Hans Buss <hans.buss@mailfence.com>
2020-05-25 10:47:27 -0300

    removed used o malloc and free

--- main.c
+++ main.c
@@ -5,11 +5,11 @@
 #include <semaphore.h>
 #include <unistd.h>
 
-#define SIZE 2
+#define SIZE 5
 
-int* buffer;
+int* buffer[SIZE];
 
-sem_t *cheio, *vazio, *lock_prod, *lock_cons;
+sem_t cheio, vazio, lock_prod, lock_cons;
 
 int f = 0;
 int i = 0;
@@ -18,16 +18,16 @@ void* producer(void* args) {
 	
 	while (1) {
 	
-		sem_wait(vazio);
-		sem_wait(lock_prod);
+		sem_wait(&vazio);
+		sem_wait(&lock_prod);
 		
 		f = (f+1) % SIZE;
 		buffer[f] = 1;
 
 		printf("produzi no indice %d ID: %d\n", f, (int)args);	
 		
-		sem_post(lock_prod);
-		sem_post(cheio);	
+		sem_post(&lock_prod);
+		sem_post(&cheio);	
 		//sleep(1);
 	}
 	pthread_exit(NULL);
@@ -36,16 +36,16 @@ void* producer(void* args) {
 void* consumer(void* args) {
 	
 	while (1) {
-		sem_wait(cheio);
-		sem_wait(lock_cons);
+		sem_wait(&cheio);
+		sem_wait(&lock_cons);
 		
 		i = (i+1)  % SIZE;
 		buffer[i] = 0;
 		
 		printf("consumi no indice %d ID: %d\n",  i, (int)args);
 		
-		sem_post(lock_cons);
-		sem_post(vazio);
+		sem_post(&lock_cons);
+		sem_post(&vazio);
 		
 
 		sleep(1);
@@ -60,17 +60,17 @@ int main(int argc, char const* argv[]) {
 		return 1;
 	}
 
-	buffer  = malloc(sizeof(int) * SIZE);
-	cheio = malloc(sizeof(sem_t)); 
-	vazio = malloc(sizeof(sem_t));
-	lock_prod = malloc(sizeof(sem_t));
-	lock_cons = malloc(sizeof(sem_t));
+	//buffer  = malloc(sizeof(int) * SIZE);
+	//cheio = malloc(sizeof(sem_t)); 
+	//vazio = malloc(sizeof(sem_t));
+	//lock_prod = malloc(sizeof(sem_t));
+	//lock_cons = malloc(sizeof(sem_t));
 
 	
-	sem_init(cheio, 0, 0);
-	sem_init(vazio, 0, SIZE);
-	sem_init(lock_prod, 0, 1);
-	sem_init(lock_cons, 0, 1);
+	sem_init(&cheio, 0, 0);
+	sem_init(&vazio, 0, SIZE);
+	sem_init(&lock_prod, 0, 1);
+	sem_init(&lock_cons, 0, 1);
 
 	int  prod = atoi(argv[1]);
 	int cons = atoi(argv[2]);
@@ -90,14 +90,14 @@ int main(int argc, char const* argv[]) {
 		pthread_join(threads[i], NULL);
 
 	}		
-	sem_destroy(cheio);	
-	sem_destroy(vazio);
-	sem_destroy(lock_prod);
-	sem_destroy(lock_cons);
+	sem_destroy(&cheio);	
+	sem_destroy(&vazio);
+	sem_destroy(&lock_prod);
+	sem_destroy(&lock_cons);
 
-	free(cheio);
-	free(vazio);
-	free(lock_prod);
-	free(lock_cons);
+	//free(cheio);
+	//free(vazio);
+	//free(lock_prod);
+	//free(lock_cons);
 	}
 
